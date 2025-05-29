5bcb27f987e944c114416d2e6e5eb90821b0cc1c
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-05-29 13:39:24 +0400

    adding comment to home view model 2025.05.29

--- Models/HomeViewModel.cs
+++ Models/HomeViewModel.cs
@@ -2,6 +2,9 @@
 
 namespace PetShop.Models
 {
+    /// <summary>
+    /// Home view model
+    /// </summary>
     public class HomeViewModel
     {
         public List<Slider> Sliders { get; set; } = new List<Slider>();
